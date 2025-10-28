84d8f40cb149d0a91215911b8c360c14569b8c8f
redisnotbluedev <ededmaude@gmail.com>
2025-10-28 18:05:55 +1030

    ok now i need to code an entire fricking proper ui framework — time to lock in

--- v0.0.0/files/lib/gui.gd
+++ v0.0.0/files/lib/gui.gd
@@ -5,6 +5,10 @@ func _init():
 	_methods["Text"] = create_label
 	_methods["add_content"] = add_content
 
+# STARCH can't access members of external classes for basically no reason so uh
+func add_content(window: OSWindow, child):
+	window.add_content(child)
+
 func create_window(position: Vector2 = Vector2.ZERO, size: Vector2 = Vector2(500, 500)):
 	var window := OSWindow.new()
 	WindowManager.get_root().add_child(window)
@@ -19,5 +23,3 @@ func create_label(text: String):
 	label.set_anchors_preset(Control.PRESET_FULL_RECT)
 	return label
 
-func add_content(window: OSWindow, child):
-	window.add_content(child)
