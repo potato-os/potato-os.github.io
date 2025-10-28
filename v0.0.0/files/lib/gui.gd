bcc95bc950d493e03f7116ade4966dbf6ad28e74
redisnotbluedev <ededmaude@gmail.com>
2025-10-28 17:35:51 +1030

    istg if this doesnt work i will actually

--- v0.0.0/files/lib/gui.gd
+++ v0.0.0/files/lib/gui.gd
@@ -1,20 +1,24 @@
-var _methods = {
-    "Window": create_window,
-    "Text": create_label
-}
+var _methods = {}
 
 func _init():
-	print("CREATING UAHDHAa!!!!!!!!!!!!!!!!")
 	_methods["Window"] = create_window
 	_methods["Text"] = create_label
+	_methods["add_content"] = add_content
 
 func create_window(position: Vector2 = Vector2.ZERO, size: Vector2 = Vector2(500, 500)):
-	print("CREATING WINDOW!!!!!!!!!!!!!!!!")
 	var window := OSWindow.new()
 	WindowManager.get_root().add_child(window)
+	window.position = position
+	window.size = size
 	return window
 
-func create_label(content):
+func create_label(text: String):	
 	var label := Label.new()
-	label.text = content
+	label.text = str(text)
+	label.add_theme_color_override("font_color", Color.BLACK)
 	return label
+
+func add_content(window: OSWindow, child):
+	print(window)
+	print(child)
+	window.add_content(child)
