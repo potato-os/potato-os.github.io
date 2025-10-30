41a323fe3229fbbfe4ae7250780e83b1647fdb47
redisnotbluedev <ededmaude@gmail.com>
2025-10-30 16:49:42 +1030

    add title

--- v0.0.0/files/lib/gui.gd
+++ v0.0.0/files/lib/gui.gd
@@ -7,7 +7,8 @@ func _init():
 	_classes["Text"] = Text
 
 class WindowWrapper extends OSWindow:
-	var _methods: Dictionary = {"add_content": _add_content}
+	var _methods: Dictionary = {"add_content": _add_content, "title": title}
+	
 	func _init(position: Vector2 = Vector2.ZERO, size: Vector2 = Vector2(500, 500)):
 		super()
 		WindowManager.get_root().add_child(self)
@@ -18,6 +19,10 @@ class WindowWrapper extends OSWindow:
 		print("ADDING CONTENT (FROM GUI MODULE)")
 		var real_child = child.unwrap() if child is GDScriptInstanceWrapper else child 
 		self.add_content(real_child)
+	
+	func title(text):
+		self.set_title(text)
+		return self
 
 class Widget extends Control:
 	var _methods: Dictionary = {}
