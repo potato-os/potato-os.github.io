84e0882121c6c17bee11fa00d52e826c885c5d43
redisnotbluedev <ededmaude@gmail.com>
2025-10-31 06:27:00 +1030

    try a callv

--- v0.0.0/files/lib/gui.gd
+++ v0.0.0/files/lib/gui.gd
@@ -68,4 +68,4 @@ class ButtonWidget extends Widget:
 	func colour(colour): colour = colour if colour is Color else colour.unwrap(); o("font_color", colour); o("font_focus_color", colour); o("font_pressed_color", colour); o("font_hover_color", colour); o("font_hover_pressed_color", colour); o("font_disabled_color", colour); return self;
 	func font_size(size: int): _button.add_theme_font_size_override("font_size", size); return self;
 	func font(path: String): var font = FontFile.new(); font.load_dynamic_font("user://potatofs".path_join(name)); _button.add_theme_font_override("font", font); return self;
-	func on_click(callback): _button.pressed.connect(callback)
+	func on_click(callback): _button.pressed.connect(func (...args): callback.callv(...args))
