cf6c0f1ab26e5d104f6365d93330a7d1833215ba
redisnotbluedev <ededmaude@gmail.com>
2025-10-31 05:13:35 +1030

    graphics lib is so epic

--- v0.0.0/files/lib/graphics.gd
+++ v0.0.0/files/lib/graphics.gd
@@ -7,12 +7,20 @@ class ColorWrapper:
 	var _color: Color
 	var _methods = {"code": code}
 
-	func _init(r, g, b, a): _color = Color(r, g, b, a)
-	func code(code): var result = ColorWrapper.new(0, 0, 0, 0); result._color = Color(code); return result
+	func _init(r=null, g=null, b=null, a=null):
+		if r is String:
+			_color = Color(r)
+		elif r is Color:
+			_color = r
+		elif r != null and g != null and b != null:
+			_color = Color(r, g, b, a if a != null else 1.0)
+		else:
+			_color = Color(0, 0, 0, 0)
+	func unwrap(): return _color
 
 class ImageWrapper:
 	var _image: Image
 	var _methods = {"file": file}
 
-	func _init(): _image = Image.new()
-	func file(path): var base = OS.get_user_data_dir().path_join("potatofs"); var new_path = base.path_join(path).simplify_path(); return Image.new().load(new_path) if new_path.begins_with(base) else null
+	func _init(path): var base = OS.get_user_data_dir().path_join("potatofs"); var new_path = base.path_join(path).simplify_path(); _image = Image.new().load(new_path) if new_path.begins_with(base) else null
+	func unwrap(): return _image
