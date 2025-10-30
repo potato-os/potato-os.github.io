2d998033e03ba3ef3933d2dcfbe790b593058059
redisnotbluedev <ededmaude@gmail.com>
2025-10-31 05:20:41 +1030

    dont actually have to expose any methods nr

--- v0.0.0/files/lib/graphics.gd
+++ v0.0.0/files/lib/graphics.gd
@@ -5,7 +5,6 @@ var _classes = {
 
 class ColorWrapper:
 	var _color: Color
-	var _methods = {"code": code}
 
 	func _init(r=null, g=null, b=null, a=null):
 		if r is String:
@@ -20,7 +19,6 @@ class ColorWrapper:
 
 class ImageWrapper:
 	var _image: Image
-	var _methods = {"file": file}
 
 	func _init(path): var base = OS.get_user_data_dir().path_join("potatofs"); var new_path = base.path_join(path).simplify_path(); _image = Image.new().load(new_path) if new_path.begins_with(base) else null
 	func unwrap(): return _image
