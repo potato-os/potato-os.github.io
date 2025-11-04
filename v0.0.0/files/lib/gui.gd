a92049bc3251c91751562d8f529ff0eeb799a374
redisnotbluedev <ededmaude@gmail.com>
2025-11-04 16:43:11 +1030

    please debug????

--- v0.0.0/files/lib/gui.gd
+++ v0.0.0/files/lib/gui.gd
@@ -92,7 +92,7 @@ class ContainerWidget extends Widget:
 	var _container: Control
 
 	func _init(): super(); _container = Control.new(); add_child(_container); _container.set_anchors_and_offsets_preset(Control.PRESET_FULL_RECT); _methods["add"] = add; _methods["remove"] = remove;
-	func add(child: Node): child.container_parent = true; _container.add_child(child); return self;
+	func add(child: Node): print("GUI: Setting %s as a container child!" % child); child.container_parent = true; print("Adding %s to %s as a child!" % [child, _container]); _container.add_child(child); print("Returning self!"); return self;
 	func remove(child: Node): _container.remove_child(child); return self;
 
 class FlexBox extends ContainerWidget:
