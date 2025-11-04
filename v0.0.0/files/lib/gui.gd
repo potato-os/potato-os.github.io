77020b1813416a523700deecc3bf3cc54a6c92bf
redisnotbluedev <ededmaude@gmail.com>
2025-11-04 17:09:54 +1030

    wtf man why u gotta betray me like that

--- v0.0.0/files/lib/gui.gd
+++ v0.0.0/files/lib/gui.gd
@@ -34,19 +34,19 @@ class Widget extends Control:
 	var container_parent: bool = false
 	
 	func _init(): _methods["fill"] = fill; _methods["dock"] = dock; _methods["width"] = width; _methods["height"] = height; _methods["get_size"] = size; _methods["get_position"] = position; _methods["anchor_left"] = set_anchor_left; _methods["anchor_right"] = set_anchor_right; _methods["anchor_top"] = set_anchor_top; _methods["anchor_bottom"] = set_anchor_bottom; _methods["margin_all"] = margin_all; _methods["get_parent"] = parent; _methods["delete"] = delete; _methods["add"] = (func(child): print("Calling ADD from lambda!"); Callable(self, "add").call(child.unwrap() if child is GDScriptInstanceWrapper else child); return self); _methods["remove"] = (func(child): print("Calling REMOVE from lambda!"); Callable(self, "remove").call(child.unwrap() if child is GDScriptInstanceWrapper else child); return self);
-	func fill(): if container_parent: return self; set_anchors_preset(Control.PRESET_FULL_RECT); return self;
-	func dock(side: int): if container_parent: return self; set_anchors_preset(Control.PRESET_TOP_WIDE if side == 0 else (Control.PRESET_LEFT_WIDE if side == 1 else (Control.PRESET_BOTTOM_WIDE if side == 2 else Control.PRESET_RIGHT_WIDE))); return self;
+	func fill(): set_anchors_preset(Control.PRESET_FULL_RECT); return self;
+	func dock(side: int): set_anchors_preset(Control.PRESET_TOP_WIDE if side == 0 else (Control.PRESET_LEFT_WIDE if side == 1 else (Control.PRESET_BOTTOM_WIDE if side == 2 else Control.PRESET_RIGHT_WIDE))); return self;
 	func width(width: int): custom_minimum_size.x = width; return self;
 	func height(height: int): custom_minimum_size.y = height; return self;
 	func size(): return self.global_size;
 	func position(): return self.global_position;
 	
-	func set_anchor_left(anchor: float): if container_parent: return self; set_anchor(SIDE_LEFT, anchor); return self;
-	func set_anchor_right(anchor: float): if container_parent: return self; set_anchor(SIDE_RIGHT, anchor); return self;
-	func set_anchor_top(anchor: float): if container_parent: return self; set_anchor(SIDE_TOP, anchor); return self;
-	func set_anchor_bottom(anchor: float): if container_parent: return self; set_anchor(SIDE_BOTTOM, anchor); return self;
+	func set_anchor_left(anchor: float): set_anchor(SIDE_LEFT, anchor); return self;
+	func set_anchor_right(anchor: float): set_anchor(SIDE_RIGHT, anchor); return self;
+	func set_anchor_top(anchor: float): set_anchor(SIDE_TOP, anchor); return self;
+	func set_anchor_bottom(anchor: float): set_anchor(SIDE_BOTTOM, anchor); return self;
 
-	func margin_all(margin: float): if container_parent: return self; set_anchor_and_offset(SIDE_TOP, anchor_top, margin); set_anchor_and_offset(SIDE_LEFT, anchor_left, margin); set_anchor_and_offset(SIDE_BOTTOM, anchor_bottom, -margin); set_anchor_and_offset(SIDE_RIGHT, anchor_right, -margin); return self;
+	func margin_all(margin: float): set_anchor_and_offset(SIDE_TOP, anchor_top, margin); set_anchor_and_offset(SIDE_LEFT, anchor_left, margin); set_anchor_and_offset(SIDE_BOTTOM, anchor_bottom, -margin); set_anchor_and_offset(SIDE_RIGHT, anchor_right, -margin); return self;
 	func expand(expand): if not container_parent: return self; var flag = SIZE_EXPAND_FILL if expand else SIZE_SHRINK_BEGIN; set_h_size_flags(flag); set_v_size_flags(flag); return self;
 
 	func add(child: Node): add_child(child); return self;
